78ce7b6c879c6b8d0e2d7abde1fc7204702a6b44
Dinesh Rout <rddinesh779@gmail.com>
2022-10-27 10:29:15 +0530

    refactor: code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import React from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import AppView from './pages/AppView/AppView'
+import reactLogo from './assets/react.svg';
+import './App.css';
+import AppView from './pages/AppView/AppView';
 
 function App() {
-
-  return (
-    <div className="App">
-      <AppView/>
-    </div>
-  )
+  return <AppView />;
 }
 
-export default App
+export default App;
--- src/pages/AppView/AppView.tsx
+++ src/pages/AppView/AppView.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import Header from "../../components/Header/Header";
-import { Grid } from "@mantine/core";
-import AddInfoSection from "../../components/AddInfoSection/AddInfoSection";
-import NoticeViewSection from "../../components/NoticeViewSection/NoticeViewSection";
+import React from 'react';
+import Header from '../../components/Header/Header';
+import { Container, Grid } from '@mantine/core';
+import AddInfoSection from '../../components/AddInfoSection/AddInfoSection';
+import NoticeViewSection from '../../components/NoticeViewSection/NoticeViewSection';
 
 function AppView() {
   return (
-    <div>
+    <>
       <Header />
-      <Grid grow justify="space-between" gutter="xl">
-        <Grid.Col span={6}>
-          <AddInfoSection />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <NoticeViewSection />
-        </Grid.Col>
-      </Grid>
-    </div>
+      <Container>
+        <Grid grow justify='space-between'>
+          <Grid.Col span={6}>
+            <AddInfoSection />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <NoticeViewSection />
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
