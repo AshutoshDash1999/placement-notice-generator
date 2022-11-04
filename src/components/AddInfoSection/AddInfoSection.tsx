2f9cbf13ed4f55dcc4b34999306f13797484a8ca
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-11-04 18:16:33 +0530

    form input added and previewed

--- src/components/AddInfoSection/AddInfoSection.tsx
+++ src/components/AddInfoSection/AddInfoSection.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { useId } from "react";
 import {
   Container,
   Button,
@@ -11,49 +11,48 @@ import {
   TextInput,
   Title,
   Select,
-} from '@mantine/core';
-import { IconPercentage } from '@tabler/icons';
-import { useForm } from '@mantine/form';
-import { useContext, useEffect, useId } from 'react';
-import InputMask from 'react-input-mask';
-import { DatePicker, TimeInput } from '@mantine/dates';
-import { addDegree, changeInput, useApp } from '../../context/AppContext';
+} from "@mantine/core";
+import { IconPercentage } from "@tabler/icons";
+import { useForm } from "@mantine/form";
+import InputMask from "react-input-mask";
+import { DatePicker, TimeInput } from "@mantine/dates";
+import { addDegree, changeInput, useApp } from "../../context/AppContext";
 
 function AddInfoSection() {
   const id = useId();
 
   const degreeData = [
-    { value: 'bsc', label: 'B.Sc' },
-    { value: 'btech', label: 'B.Tech' },
-    { value: 'mca', label: 'MCA' },
-    { value: 'msc', label: 'M.Sc' },
-    { value: 'mtech', label: 'M.Tech' },
+    { value: "B.Sc", label: "B.Sc" },
+    { value: "B.Tech", label: "B.Tech" },
+    { value: "MCA", label: "MCA" },
+    { value: "M.Sc", label: "M.Sc" },
+    { value: "M.Tech", label: "M.Tech" },
   ];
 
   const branchData = [
-    { value: 'cse', label: 'CSE' },
-    { value: 'it', label: 'IT' },
-    { value: 'ee', label: 'EE' },
-    { value: 'ie', label: 'I&E' },
-    { value: 'ft', label: 'FT' },
-    { value: 'te', label: 'TE' },
-    { value: 'ce', label: 'CE' },
-    { value: 'bt', label: 'BT' },
-    { value: 'mca', label: 'MCA' },
-    { value: 'chem', label: 'Chem' },
-    { value: 'physics', label: 'Physics' },
-    { value: 'mc', label: 'Maths&Comp.' },
+    { value: "CSE", label: "CSE" },
+    { value: "IT", label: "IT" },
+    { value: "EE", label: "EE" },
+    { value: "I&E", label: "I&E" },
+    { value: "FT", label: "FT" },
+    { value: "TE", label: "TE" },
+    { value: "CE", label: "CE" },
+    { value: "BT", label: "BT" },
+    { value: "MCA", label: "MCA" },
+    { value: "Chem", label: "Chem" },
+    { value: "Physics", label: "Physics" },
+    { value: "Maths&Comp.", label: "Maths&Comp." },
   ];
 
   const maxBacklogs = [
-    { value: '0', label: '0' },
-    { value: '1', label: '1' },
-    { value: '2', label: '2' },
-    { value: '3', label: '3' },
-    { value: '4', label: '4' },
-    { value: '5', label: '5' },
-    { value: '6', label: '6' },
-    { value: '7', label: '7' },
+    { value: "0", label: "0" },
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+    { value: "5", label: "5" },
+    { value: "6", label: "6" },
+    { value: "7", label: "7" },
   ];
 
   // const [placementNoticeData, setPlacementNoticeData] = useState({
@@ -102,89 +101,92 @@ function AddInfoSection() {
 
   return (
     <Container>
-      <Title order={3} mb='sm' weight={400}>
+      <Title order={3} mb="sm" weight={400}>
         Placement Details
       </Title>
 
       <form onSubmit={formSubmit}>
-        <Button type='submit'>Clcik</Button>
+        {/* <Button type='submit'>Clcik</Button> */}
 
         {/* notice id  */}
         <TextInput
-          name='notice_id'
-          mb='xs'
+          name="notice_id"
+          mb="xs"
           id={id}
           required
-          label='Placement Notice ID'
-          placeholder='PA-XXXX'
-          description='Write the notice number here'
+          label="Placement Notice ID"
+          placeholder="PA-XXXX"
+          description="Write the notice number here"
           defaultValue={state.notice_id}
           onChange={handleInput}
         />
 
         {/* company name  */}
         <TextInput
-          name='company_name'
-          mb='xs'
+          name="company_name"
+          mb="xs"
           id={id}
           required
-          label='Company Name'
-          placeholder=''
-          description='Write company name here'
+          label="Company Name"
+          placeholder=""
+          description="Write company name here"
           defaultValue={state.company_name}
           onChange={handleInput}
+          // {...form.getInputProps('company_name')}
         />
 
         {/* about company  */}
         <Textarea
-          mb='xs'
-          placeholder='About Company'
-          label='About Company'
-          name='about_company'
+          id={id}
+          mb="xs"
+          placeholder="About Company"
+          label="About Company"
+          name="about_company"
           withAsterisk
           required
-          description='Write about company here'
+          description="Write about company here"
           defaultValue={state.about_company}
           onChange={handleInput}
+          // {...form.getInputProps('about_company')}
         />
 
         {/* job role  */}
         <div>
           <TextInput
-            mb='xs'
-            label='Job Role'
-            placeholder='Full Stack Developer'
-            name='job_role'
+            mb="xs"
+            label="Job Role"
+            placeholder="Full Stack Developer"
+            name="job_role"
             defaultValue={state.job_role}
             onChange={handleInput}
             withAsterisk
           />
           <TextInput
-            mb='xs'
-            label='CTC'
-            name='ctc'
-            placeholder='6'
-            defaultValue={state.job_role}
+            mb="xs"
+            label="CTC"
+            name="ctc"
+            placeholder="6"
+            defaultValue={state.ctc}
             onChange={handleInput}
             withAsterisk
           />
-          <Button variant='light' my='xs'>
+          <Button variant="light" my="xs">
             Add New Role
           </Button>
         </div>
 
         {/* eligibility criteria  */}
         <div>
-          <Text size='md'>Eligibility Criteria:</Text>
-          <List type='ordered'>
+          <Text size="md">Eligibility Criteria:</Text>
+          <List type="ordered">
             <List.Item>
               <MultiSelect
-                name='degree_allowed'
+                name="degree_allowed"
                 data={degreeData}
                 value={state.degree_allowed}
                 onChange={handleDropDown}
-                label='Degree Allowed'
-                placeholder='Pick degree'
+                label="Degree Allowed"
+                placeholder="Pick degree"
                 searchable
                 clearable
                 required
@@ -192,81 +194,106 @@ function AddInfoSection() {
             </List.Item>
             <List.Item>
               <MultiSelect
+                name="branches_allowed"
                 data={branchData}
-                label='Branches Allowed'
-                placeholder='Pick branch'
+                value={state.branches_allowed}
+                onChange={handleDropDown}
+                label="Branches Allowed"
+                placeholder="Pick branch"
                 searchable
+                clearable
                 required
               />
             </List.Item>
             <List.Item>
               <Select
-                label='Maximum backlogs allowed'
-                placeholder='Pick backlogs count'
+                name="max_backlog"
+                label="Maximum backlogs allowed"
+                placeholder="Pick backlogs count"
                 data={maxBacklogs}
                 searchable
                 required
+                value={state.max_backlog}
+                clearable
               />
             </List.Item>
             <List.Item>
-              <Input.Wrapper id={id} label='10th Percentage'>
+              <Input.Wrapper id={id} label="10th Percentage">
                 <Input
                   id={id}
-                  placeholder='10th percentage'
+                  placeholder="10th percentage"
+                  name="tenth_perc"
                   rightSection={<IconPercentage />}
+                  defaultValue={state.tenth_perc}
+                  onChange={handleInput}
                 />
               </Input.Wrapper>
             </List.Item>
             <List.Item>
-              <Input.Wrapper id={id} label='12th Percentage'>
+              <Input.Wrapper id={id} label="12th Percentage">
                 <Input
+                  name="twelfth_perc"
                   id={id}
-                  placeholder='12th percentage'
+                  placeholder="12th percentage"
                   rightSection={<IconPercentage />}
+                  defaultValue={state.twelfth_perc}
+                  onChange={handleInput}
                 />
               </Input.Wrapper>
             </List.Item>
             <List.Item>
-              <Input.Wrapper id={id} label='Diploma Percentage'>
+              <Input.Wrapper id={id} label="Diploma Percentage">
                 <Input
+                  name="diploma_perc"
                   component={InputMask}
-                  mask='99'
+                  mask="99"
                   id={id}
-                  placeholder='Diploma percentage'
+                  placeholder="Diploma percentage"
                   rightSection={<IconPercentage />}
+                  defaultValue={state.diploma_perc}
+                  onChange={handleInput}
                 />
               </Input.Wrapper>
             </List.Item>
             <List.Item>
-              <Input.Wrapper id={id} label='UG Percentage'>
+              <Input.Wrapper id={id} label="UG Percentage">
                 <Input
+                  name="ug_perc"
                   component={InputMask}
-                  mask='99'
+                  mask="99"
                   id={id}
-                  placeholder='UG percentage'
+                  placeholder="UG percentage"
                   rightSection={<IconPercentage />}
+                  defaultValue={state.ug_perc}
+                  onChange={handleInput}
                 />
               </Input.Wrapper>
             </List.Item>
             <List.Item>
-              <Input.Wrapper id={id} label='PG Percentage'>
+              <Input.Wrapper id={id} label="PG Percentage">
                 <Input
+                  name="pg_perc"
                   component={InputMask}
-                  mask='99'
+                  mask="99"
                   id={id}
-                  placeholder='PG percentage'
+                  placeholder="PG percentage"
                   rightSection={<IconPercentage />}
+                  defaultValue={state.pg_perc}
+                  onChange={handleInput}
                 />
               </Input.Wrapper>
             </List.Item>
             <List.Item>
-              <Input.Wrapper id={id} label='Minimum Gap year'>
+              <Input.Wrapper id={id} label="Minimum Gap year">
                 <Input
+                  name="min_gap"
                   component={InputMask}
-                  mask='9'
+                  mask="9"
                   id={id}
-                  placeholder='Minimum year of gaps'
+                  placeholder="Minimum year of gaps"
                   rightSection={<IconPercentage />}
+                  defaultValue={state.min_gap}
+                  onChange={handleInput}
                 />
               </Input.Wrapper>
             </List.Item>
@@ -275,19 +302,31 @@ function AddInfoSection() {
 
         {/* form apply  */}
         <div>
-          <TextInput label='Form Link' withAsterisk />
+          <TextInput
+            name="form_link"
+            defaultValue={state.form_link}
+            onChange={handleInput}
+            label="Form Link"
+            withAsterisk
+            placeholder="Paste google form link here"
+          />
           <DatePicker
-            placeholder='Pick date'
-            label='Form submission date'
+            name="form_submission_date"
+            placeholder="Pick date"
+            label="Form submission date"
             withAsterisk
             clearable
+            defaultValue={state.form_submission_date}
+            onChange={handleInput}
           />
           <TimeInput
-            label='Pick time'
-            format='12'
-            defaultValue={new Date()}
+            name="form_submission_time"
+            label="Pick time"
+            format="12"
             withAsterisk
             clearable
+            defaultValue={state.form_submission_time}
+            onChange={handleInput}
           />
         </div>
       </form>
--- src/components/NoticeViewSection/NoticeViewSection.tsx
+++ src/components/NoticeViewSection/NoticeViewSection.tsx
@@ -6,32 +6,33 @@ import {
   Tabs,
   Title,
   Tooltip,
-} from '@mantine/core';
-import { useApp } from '../../context/AppContext';
+} from "@mantine/core";
+import { useApp } from "../../context/AppContext";
 
 function NoticeViewSection() {
   const { state: values } = useApp();
-
+  console.log(values);
+  
   return (
     <Container>
       <Title order={3} weight={400}>
         Notice View
       </Title>
 
-      <Tabs defaultValue='preview' mt={'sm'}>
+      <Tabs defaultValue="preview" mt={"sm"}>
         <Tabs.List>
-          <Tabs.Tab value='preview'>Preview</Tabs.Tab>
-          <Tabs.Tab value='text'>Text</Tabs.Tab>
+          <Tabs.Tab value="preview">Preview</Tabs.Tab>
+          <Tabs.Tab value="text">Text</Tabs.Tab>
         </Tabs.List>
 
-        <Tabs.Panel value='preview' pt='xs'>
+        <Tabs.Panel value="preview" pt="xs">
           <Box
             sx={{
-              margin: '1em 0',
-              border: '1px solid #ced4da',
-              minHeight: '200px',
-              borderRadius: '4px',
-              padding: '8px',
+              margin: "1em 0",
+              border: "1px solid #ced4da",
+              minHeight: "500px",
+              borderRadius: "4px",
+              padding: "8px",
             }}
           >
             {/* notice id */}
@@ -52,17 +53,92 @@ function NoticeViewSection() {
                 {values.about_company}
               </p>
             )}
+            {values.job_role && (
+              <p>
+                <b>Job Role: </b> <br />
+                {values.job_role}
+              </p>
+            )}
+            {values.ctc && (
+              <p>
+                <b>CTC: </b> <br />
+                {values.ctc}
+              </p>
+            )}
+            {values.degree_allowed.length > 0 && (
+              <p>
+                <b>Degree: </b> <br />
+                {values.degree_allowed.toString()}
+              </p>
+            )}
+            {values.branches_allowed.length > 0 && (
+              <p>
+                <b>Branch: </b> <br />
+                {values.branches_allowed}
+              </p>
+            )}
+
+            {values.max_backlog && (
+              <p>
+                <b>Maximum Backlog: </b> <br />
+                {values.max_backlog}
+              </p>
+            )}
+
+            {values.tenth_perc && (
+              <p>
+                <b>Min 10th %: </b> <br />
+                {values.tenth_perc}%
+              </p>
+            )}
+
+            {values.twelfth_perc && (
+              <p>
+                <b>Min 12th %: </b> <br />
+                {values.twelfth_perc}%
+              </p>
+            )}
+            
+            {values.diploma_perc && (
+              <p>
+                <b>Min diploma %: </b> <br />
+                {values.diploma_perc}%
+              </p>
+            )}
+            
+            {values.ug_perc && (
+              <p>
+                <b>Min UG %: </b> <br />
+                {values.ug_perc}%
+              </p>
+            )}
+            
+            {values.pg_perc && (
+              <p>
+                <b>Min PG %: </b> <br />
+                {values.pg_perc}%
+              </p>
+            )}
+
+            {
+              values.form_link && (
+                <p>
+                  All the eligible and interested candidates are required to fill the form by : <b>{values.form_link}</b>
+                </p>
+              )
+            }
+
           </Box>
         </Tabs.Panel>
 
-        <Tabs.Panel value='text' pt='xs'>
+        <Tabs.Panel value="text" pt="xs">
           <Box
             sx={{
-              margin: '1em 0',
-              border: '1px solid #ced4da',
-              minHeight: '200px',
-              borderRadius: '4px',
-              padding: '8px',
+              margin: "1em 0",
+              border: "1px solid #ced4da",
+              minHeight: "500px",
+              borderRadius: "4px",
+              padding: "8px",
             }}
           >
             {/* these should be according to whatsapp formatting */}
@@ -73,13 +149,13 @@ function NoticeViewSection() {
         </Tabs.Panel>
       </Tabs>
 
-      <Tooltip label='Copy formatted text'>
+      <Tooltip label="Copy formatted text">
         {/* value is the formatted text */}
         {/* TODO: generate the formatted text from the values(coming from global state)*/}
-        <CopyButton value='text_to_be_copied'>
+        <CopyButton value="text_to_be_copied">
           {({ copied, copy }) => (
-            <Button color={copied ? 'teal' : 'blue'} onClick={copy}>
-              {copied ? 'Copied text' : 'Copy'}
+            <Button color={copied ? "teal" : "blue"} onClick={copy}>
+              {copied ? "Copied text" : "Copy"}
             </Button>
           )}
         </CopyButton>
