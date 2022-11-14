108518d0fdb59259022a97449aae5ba9fa413ac2
Dinesh Rout <rddinesh779@gmail.com>
2022-11-15 01:19:10 +0530

    feat: add/remove jobs

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,12 +1,19 @@
-import { createContext, useReducer, useContext, type Dispatch } from "react";
+import { createContext, useReducer, useContext, type Dispatch } from 'react';
+import { nanoid } from 'nanoid';
+
+type jobType = {
+  id: string;
+  title: string;
+  ctc: string;
+};
 
 type InitialStateType = {
   notice_id: string;
   company_name: string;
   company_website: string;
   about_company: string;
-  job_role: string;
-  ctc: string;
+  // job: Array<{ job_role: string; ctc: string }>;
+  jobs: jobType[];
   bond: boolean;
   bond_period: string;
   // eligibility criteria
@@ -28,30 +35,29 @@ type InitialStateType = {
 };
 
 const initialState = {
-  notice_id: "",
-  company_name: "",
-  company_website: "",
-  about_company: "",
-  job_role: "",
-  ctc: "",
+  notice_id: '',
+  company_name: '',
+  company_website: '',
+  about_company: '',
+  jobs: [{ id: nanoid(), title: '', ctc: '' }],
   bond: false,
-  bond_period: "",
+  bond_period: '',
   // eligibility criteria
   degree_allowed: [],
   branches_allowed: [],
-  max_backlog: "",
-  tenth_perc: "",
-  twelfth_perc: "",
-  diploma_perc: "",
-  ug_perc: "",
-  pg_perc: "",
-  min_gap: "",
+  max_backlog: '',
+  tenth_perc: '',
+  twelfth_perc: '',
+  diploma_perc: '',
+  ug_perc: '',
+  pg_perc: '',
+  min_gap: '',
 
   // other details:
-  form_link: "",
+  form_link: '',
   form_submission_date: new Date(),
   form_submission_time: new Date(),
-  extra_note: "",
+  extra_note: '',
 };
 
 const AppContext = createContext<{
@@ -64,49 +70,70 @@ const AppContext = createContext<{
 
 export function changeInput(payload: any) {
   return {
-    type: "CHANGE_INPUT",
+    type: 'CHANGE_INPUT',
+    payload,
+  };
+}
+
+export function changeJobInput(payload: any) {
+  return {
+    type: 'CHANGE_JOB_INPUT',
+    payload,
+  };
+}
+
+export function addJob() {
+  return {
+    type: 'ADD_JOB',
+    payload: null,
+  };
+}
+
+export function removeJob(payload: any) {
+  return {
+    type: 'REMOVE_JOB',
     payload,
   };
 }
 
 export function addDegree(payload: any) {
   return {
-    type: "PUSH_DEGREE",
+    type: 'PUSH_DEGREE',
     payload,
   };
 }
 
 export function addBranch(payload: any) {
   return {
-    type: "PUSH_BRANCH",
+    type: 'PUSH_BRANCH',
     payload,
   };
 }
 
 export function addBacklogs(payload: any) {
   return {
-    type: "PUSH_BACKLOGS",
+    type: 'PUSH_BACKLOGS',
     payload,
   };
 }
 
 export function addBond(payload: any) {
   return {
-    type: "PUSH_BOND",
+    type: 'PUSH_BOND',
     payload,
   };
 }
 
 export function addDate(payload: any) {
   return {
-    type: "PUSH_DATE",
+    type: 'PUSH_DATE',
     payload,
   };
 }
 
 export function addTime(payload: any) {
   return {
-    type: "PUSH_TIME",
+    type: 'PUSH_TIME',
     payload,
   };
 }
@@ -120,12 +147,41 @@ export function addTime(payload: any) {
 
 const reducers = (state: any, action: any) => {
   switch (action.type) {
-    case "CHANGE_INPUT":
+    case 'CHANGE_INPUT':
       return {
         ...state,
         ...action.payload,
       };
-    case "PUSH_DEGREE":
+    case 'ADD_JOB':
+      return {
+        ...state,
+        jobs: [
+          ...state.jobs,
+          {
+            id: nanoid(),
+            title: '',
+            ctc: '',
+          },
+        ],
+      };
+    case 'REMOVE_JOB':
+      return {
+        ...state,
+        jobs: state.jobs.filter((job: jobType) => job.id !== action.payload.id),
+      };
+    case 'CHANGE_JOB_INPUT':
+      return {
+        ...state,
+        jobs: state.jobs.map((job: jobType) => {
+          if (job.id === action.payload.id) {
+            return {
+              ...job,
+              [action.payload.values.name]: action.payload.values.value,
+            };
+          }
+        }),
+      };
+    case 'PUSH_DEGREE':
       return {
         ...state,
         degree_allowed: action.payload,
@@ -135,27 +191,27 @@ const reducers = (state: any, action: any) => {
     //     ...state,
     //     degree_allowed: action.payload,
     //   };
-    case "PUSH_BRANCH":
+    case 'PUSH_BRANCH':
       return {
         ...state,
         branches_allowed: action.payload,
       };
-    case "PUSH_BACKLOGS":
+    case 'PUSH_BACKLOGS':
       return {
         ...state,
         max_backlog: action.payload,
       };
-    case "PUSH_BOND":
+    case 'PUSH_BOND':
       return {
         ...state,
         bond: action.payload,
       };
-    case "PUSH_DATE":
+    case 'PUSH_DATE':
       return {
         ...state,
         form_submission_date: action.payload,
       };
-    case "PUSH_TIME":
+    case 'PUSH_TIME':
       return {
         ...state,
         form_submission_time: action.payload,
